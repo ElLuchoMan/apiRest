ad3f84392f53c58d627f8a9d38267ae496b3d244
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-05-07 21:35:18 -0500

    Agregado método listar todo

--- src/main/java/com/example/demo/dao/UserController.java
+++ src/main/java/com/example/demo/dao/UserController.java
@@ -1,13 +1,19 @@
 package com.example.demo.dao;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +41,38 @@ public class UserController {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(oUser);
+	}
+
+	// actualizar user
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@RequestBody User userDetails, @PathVariable(value = "id") Long userId) {
+		Optional<User> user = userService.findById(userId);
+		if (!user.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		// BeanUtils.copyProperties(userDetails, user.get());
+		user.get().setName(userDetails.getName());
+		user.get().setLastname(userDetails.getLastname());
+		user.get().setEmail(userDetails.getEmail());
+		user.get().setEnabled(userDetails.getEnabled());
+		return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user.get()));
+	}
+
+	// Borrar usuario
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long userId) {
+		if (!userService.findById(userId).isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		userService.deleteById(userId);
+		return ResponseEntity.ok().build();
+	}
+
+	// Leer todos los usuarios
+	@GetMapping
+	public List<User> readAll() {
+		List<User> users = StreamSupport.stream(userService.findAll().spliterator(), false)
+				.collect(Collectors.toList());
+		return users;
 	}
 }
