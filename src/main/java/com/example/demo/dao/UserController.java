ee22a858e3608484db26e2e612d9265fd4e984c1
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-05-09 18:05:45 -0500

    Se agrega cors

--- src/main/java/com/example/demo/dao/UserController.java
+++ src/main/java/com/example/demo/dao/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
-
+@CrossOrigin(origins ="http://localhost:4200/",maxAge=3600)
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -34,6 +35,7 @@ public class UserController {
 	}
 
 	// Leer usuario
+	
 	@GetMapping("/{id}")
 	public ResponseEntity<?> read(@PathVariable(value = "id") Long userId) {
 		Optional<User> oUser = userService.findById(userId);
